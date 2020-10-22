e815a78a377fb20ee531cde3cd0ad58640ef5149
nileshhazra <nileshhz99@gmail.com>
2020-10-23 02:00:49 +0530

    add count and Quiz feature

--- src/com/nileshhazra/Main.java
+++ src/com/nileshhazra/Main.java
@@ -19,5 +19,27 @@ public class Main {
         byte rem5 = scanner.nextByte();
         int age = (rem3*70 + rem4*21 + rem5*15)% 105;
         System.out.println("Your age is "+ age+"; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        byte number = scanner.nextByte();
+        for (int i = 0; i<=number; i++)
+            System.out.println(i+"!");
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Q. Why do we use methods?");
+        System.out.println("1. Repeat a statement several times.");
+        System.out.println("2. To decompose a program into several subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+        byte option = scanner.nextByte();
+        while (true){
+            if(option == 2){
+                System.out.println("Congratulations! Have a nice day.");
+                break;
+            }
+            else {
+                System.out.println("Please try again!");
+                option = scanner.nextByte();
+            }
+
+        }
     }
 }
